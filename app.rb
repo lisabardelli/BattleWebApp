94d56b89339bc18740980c6881d4392d44a9bd5c
Lisa <bardelli.lisa@gmail.com>
2020-12-08 18:25:40 +0000

    Enter player names

--- app.rb
+++ app.rb
@@ -2,24 +2,18 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Testing infrastructure working!'
-  end
-
-  run! if app_file == $0
-
-
   get "/" do
+    'Testing infrastructure working!'
     erb(:index)
   end
 
-
-  post "/names" do
-    @player_1_name = params[:player_1]
-    @player_2_name = params[:player_2]
-  erb(:play)
-
+  post '/names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
   end
+
+  run! if app_file == $0
 
 end
 
